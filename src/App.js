4d11262ccffec9bf4c58d3e99a5f42a8533880fa
Stephan Procopii <stephanp@live.unc.edu>
2023-03-01 13:28:27 -0500

    testing freecodecamp portfolio template

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <About />
       <Projects />
       <Skills />
-      <Testimonials />
       <Contact />
     </main>
   );
